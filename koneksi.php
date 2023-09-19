668df74c9581150cf7ff7b2fb439dc26303050ed
RifqiNotWibu <rifqifadhillahh@gmail.com>
2023-09-20 00:09:13 +0700

    Nested koneksi.php into getConnection() function

--- koneksi.php
+++ koneksi.php
@@ -1,14 +1,16 @@
 <?php
+function getConnection() {
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "api_perbankan";
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "api_perbankan";
+    $connection = mysqli_connect($servername, $username, $password, $database);
 
-$connection = mysqli_connect($servername, $username, $password, $database);
+    if (!$connection) {
+        die("Waduh, koneksi ke database gagal nih : ". mysqli_connect_error());
+    }
 
-if (!$connection) {
-    die("Koneksi gagal : ". mysqli_connect_error());
+    return $connection;
 }
-
 ?>
