aa10a42ece8d926fdd25c6cf924df76682dded7b
RifqiNotWibu <rifqifadhillahh@gmail.com>
2023-09-20 00:10:36 +0700

    Added transfer.php (routing and error handling)

---
+++ transfer.php
@@ -0,0 +1,23 @@
+<?php
+header("Content-Type: application/json");
+
+include_once 'koneksi.php';
+include_once 'controllers/TransferController.php';
+
+$response = ["status" => "error", "message" => "Invalid request"];
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $fromAccountId = $_POST['fromAccountId'] ?? null;
+    $toAccountId = $_POST['toAccountId'] ?? null;
+    $amount = $_POST['amount'] ?? null;
+
+    if ($fromAccountId && $toAccountId && $amount) {
+        $transferController = new TransferController($connection);
+        $response = $transferController->makeTransfer($fromAccountId, $toAccountId, $amount);
+    } else {
+        $response["message"] = "Incomplete data";
+    }
+}
+
+echo json_encode($response);
+?>
